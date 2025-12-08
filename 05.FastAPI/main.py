7d73048a111f86d4f3293a48e0bbca2c97ed8c8e
Roberto Jacobo <robjacobox@gmail.com>
2025-12-08 00:26:40 -0600

    fastapi updated

--- 05.FastAPI/main.py
+++ 05.FastAPI/main.py
@@ -1,8 +1,35 @@
 from typing import Union
 from fastapi import FastAPI
+from datetime import datetime
+import zoneinfo
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-  return {"Hello": "World"}
+async def read_root():
+    return {"Hello": "World!"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+country_timezones = {
+    "CO": "America/Bogota",
+    "MX": "America/Mexico_City",
+    "AR": "America/Argentina/Buenos_Aires",
+    "BR": "America/Sao_Paulo",
+    "PE": "America/Lima",
+}
+
+@app.get("/time/{iso_code}")
+async def get_time(iso_code: str):
+  # Co => CO
+  iso = iso_code.upper()
+  timezone_str = country_timezones.get(iso)
+
+  if not timezone_str:
+    return {"error": f"Country code '{iso}' not supported"}
+
+  tz = zoneinfo.ZoneInfo(timezone_str)
+  return {"time": datetime.now(tz)}
